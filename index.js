08c0a31768e0d0f17287b3ceb7f8b61e4cf8cafe
sumit kumar <sumit1523@gmail.com>
2023-06-08 03:20:50 +0530

    monogo db connected

--- index.js
+++ index.js
@@ -4,7 +4,8 @@ const mongoose = require("mongoose");
 const typeDefs = require("./graphql/typeDefs");
 const resolvers = require("./graphql/resolvers");
 
-const MONGODB = ""; // Add you mongodb url
+const MONGODB =
+  "mongodb+srv://sumit1523:9430292802@cluster0.jsvjdm3.mongodb.net/?retryWrites=true&w=majority"; // Add you mongodb url
 
 const server = new ApolloServer({
   typeDefs,
